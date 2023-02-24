a3f0f294989ae9477375c521fdbfe170513528f5
gitpacho <crepachonpa@unal.edu.co>
2023-02-24 15:11:46 -0500

    ejercicios tipos de datos y operadores

--- EJERCICIOS/02_TiposDeDatos_Y_Operadores.py
+++ EJERCICIOS/02_TiposDeDatos_Y_Operadores.py
@@ -10,7 +10,36 @@
     un conjunto en una misma linea
 '''
 
-2) Resolver con los operadores que considere conveniente
+"2) Resolver con los operadores que considere conveniente"
+
+""" Dadas las coordenadas P1(5,4,5) y P2(0,10,9).
+Realice un codigo que determine la distancia entre ambos puntos """
+
+
+""" La calificación de informatica se encuentra en el intervalo [0,5] y se calcula tomando 4 notas, 
+con porcentajes de 15%, 25%, 20% y 40%. Si un estudiante tiene las primeras 3 calificaciones definidas.
+Realice un algoritmo que calcule la nota necesaria de la última nota para pasar la materia. """
+
+
+""" Dos automoviles realizan una carrera. El primero de ellos acelera a razón constante de 3m/s², el segundo 
+a razón de 5m/s². Si el segundo de ellos empieza su recorrido 10 segundos después que el primero ha empezado.
+¿Cuanto tiempo habrá transcurrido cuando ambos se encuentran? """
+
+
+""" Cuatro compañeros, contratan un taxi con el objeto de movilizarse juntos a la universidad. 
+El contrato es de lunes a viernes, y deben pagar al taxista $15000 por cada trayecto. El servicio se
+prestará solo de ida.
+Sin embargo, los cuatro no se movilizan juntos todos los dias. Por tanto, han planteado que la tarifa
+debe dividirse entre el numero de compañeros que se movilicen en cada trayecto.En caso, de que ninguno
+utilice el servicio. Deben pagar al taxista una tarifa de $10000, repartidos equitativamente entre los cuatro.
+A continueación veamos el uso del servicio por parte de los compañeros en la última semana de clases:
+            LUNES   MARTES  MIERCOLES   JUEVES  VIERNES
+JUAN          Si        Si        No        Si    No
+CAMILA        Si        Si        No        No    No
+JOSE          Si        No        Si        No    No
+MARIA         Si        Si        Si        No    No      
+¿Cuanto debe pagar cada estudiante? """
+
 
 
 
