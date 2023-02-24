c10c6d9490651698714f7658b15bd9cb94594b1b
gitpacho <crepachonpa@unal.edu.co>
2023-02-24 15:06:56 -0500

    ejercicios

---
+++ EJERCICIOS/02_TiposDeDatos_Y_Operadores.py
@@ -0,0 +1,21 @@
+''' 
+1) Imprimir en la terminal
+    dos enteros en una misma linea
+    dos flotantes en una misma linea
+    dos strings en una misma linea
+    dos booleanos en una misma linea
+    dos listas en una misma linea
+    dos tuplas en una misma linea
+    un diccionario en una misma linea
+    un conjunto en una misma linea
+'''
+
+2) Resolver con los operadores que considere conveniente
+
+
+
+
+
+
+
+
