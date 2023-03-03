0da62595ba1f4f6b53658fc0b4e71f45c2ec0842
gitpacho <crepachonpa@unal.edu.co>
2023-03-03 15:08:03 -0500

    ejercicios metodos de strings

--- EJERCICIOS/04_Metodos.py
+++ EJERCICIOS/04_Metodos.py
@@ -49,12 +49,12 @@ PÁRRAFO 3: ABRIENDO SU BOCA, LO DEJA ESCAPAR, Y CORRIENTE ARRIBA, LO HA VISTO N
 
 #Métodos de las listas
 lista = [1,2,3,4]
-print(dir(lista))
+#print(dir(lista))
 
 #Metodos de las tuplas
 tupla = (10,20,30)
-print(dir(tupla))
+#print(dir(tupla))
 
 #Metodos de los diccionarios
 diccionario = {1:"uno", 2:"dos", 3:"tres"}
-print(dir(diccionario))
+#print(dir(diccionario))
