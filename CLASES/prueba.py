a722fff11e1f9e2811b1d2a347c0c9f2cac740f3
gitpacho <crepachonpa@unal.edu.co>
2023-03-10 15:12:04 -0500

    ejercicios diccionarios

---
+++ CLASES/prueba.py
@@ -0,0 +1,13 @@
+diccionario = {
+    1 : "uno",
+    2 : "dos",
+    3 : "tres",
+    4 : "cuatro",
+    5 : "cinco"
+}
+print(diccionario.keys())
+print(diccionario.values())
+print(diccionario.get(6))
+diccionario.pop(1)
+print(diccionario)
+print(diccionario[2])
