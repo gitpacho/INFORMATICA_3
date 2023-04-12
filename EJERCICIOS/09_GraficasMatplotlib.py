47d7b0079c4e5bf1ce54e0ed5aa6b175cd3234d1
gitpacho <crepachonpa@unal.edu.co>
2023-04-12 14:47:38 -0500

    updates

--- EJERCICIOS/09_GraficasMatplotlib.py
+++ EJERCICIOS/09_GraficasMatplotlib.py
@@ -1,18 +1,18 @@
-columnas = ["Nombre","Cargo","Salario"]
+columnas = ["Nombre","Cargo","Salario", "Años_experiencia"]
 indices = ["0001","0002","0003","0004","0005","0006","0007","0008","0009","0010","0011","0012","0013"]
-datos =  [["Cristian Pachon"     ,"Ingeniero"      ,3200000],
-          ["Daniela Pineda"      ,"Programador"    ,4300000],
-          ["Esteban Murcia"      ,"Programador"    ,4600000],
-          ["Jose Guzman"         ,"Ingeniero"      ,3900000],
-          ["Camilo Rodriguez"    ,"Ensamblador"    ,1200000],
-          ["Mariana Londoño"     ,"Ensamblador"    ,1100000],
-          ["Estefania Muños"     ,"Ensamblador"    ,1700000],
-          ["Cristian Rodriguez"  ,"Ingeniero"      ,3100000],
-          ["Natalia Alzate"      ,"Ensamblador"    ,2200000],
-          ["Juan Sanabria"       ,"Diseñador"      ,5100000],
-          ["Juanita Calderon"    ,"Ensamblador"    ,1300000],
-          ["Laura Quintero"      ,"Administrador"  ,2500000],
-          ["Viviana Quesada"     ,"Guardia"        ,1500000]]
+datos =  [["Cristian Pachon"     ,"Ingeniero"      ,3200000, 8],
+          ["Daniela Pineda"      ,"Programador"    ,4300000, 9],
+          ["Esteban Murcia"      ,"Programador"    ,4600000, 10],
+          ["Jose Guzman"         ,"Ingeniero"      ,3900000, 8],
+          ["Camilo Rodriguez"    ,"Ensamblador"    ,1200000, 2],
+          ["Mariana Londoño"     ,"Ensamblador"    ,1100000, 1],
+          ["Estefania Muños"     ,"Ensamblador"    ,1700000, 5],
+          ["Cristian Rodriguez"  ,"Ingeniero"      ,3100000, 8],
+          ["Natalia Alzate"      ,"Ensamblador"    ,2200000, 6],
+          ["Juan Sanabria"       ,"Diseñador"      ,5100000, 11],
+          ["Juanita Calderon"    ,"Ensamblador"    ,1300000, 4],
+          ["Laura Quintero"      ,"Administrador"  ,2500000, 7],
+          ["Viviana Quesada"     ,"Guardia"        ,1500000, 3]]
 
 import pandas
 dataFrameEmpresa = pandas.DataFrame(index=indices,
@@ -20,3 +20,5 @@ dataFrameEmpresa = pandas.DataFrame(index=indices,
                                     data=datos
                                     )
 print(dataFrameEmpresa)
+
+
