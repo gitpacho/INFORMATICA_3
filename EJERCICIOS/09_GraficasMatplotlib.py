5002cce9d32ab4c64cb854d4421f166f598ad361
gitpacho <crepachonpa@unal.edu.co>
2023-04-12 16:52:51 -0500

    presiciones graficas

--- EJERCICIOS/09_GraficasMatplotlib.py
+++ EJERCICIOS/09_GraficasMatplotlib.py
@@ -72,7 +72,7 @@ plt.savefig("EJERCICIOS/figura3.png")
 
 #Ejercicio1 ===>  
 """
-Realizar un diagrama de barras del rendimiento vs edad
+Realizar un diagrama de barras del rendimiento y un histograma de la edad
 -------------- Deportista_1  Deportista_2  Deportista_3  Deportista_4  Deportista_5  Deportista_6  Deportista_7  Deportista_8  Deportista_9  Deportista_10  Deportista_11  Deportista_12  Deportista_13  Deportista_14  Deportista_15  Deportista_16  Deportista_17  Deportista_18  Deportista_19  Deportista_20  Deportista_21  Deportista_22  Deportista_23  Deportista_24  Deportista_25  Deportista_26  Deportista_27 
 Rendimiento --    "A"           "B"           "C"            "B"            "B"           "B"          "C"           "B"             "A"           "C"          "B"          "A"             "C"             "B"          "B"              "B"           "B"             "A"           "B"            "A"           "A"             "C"             "B"             "B"          "B"            "B"            "C"             
 Edad ---------     42            60            18             20             35            25           60            30              19            42           21           17              39              30           28               34            27              23            23             20            25             "40"             31             32            45             26             17             
